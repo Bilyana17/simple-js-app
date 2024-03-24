63880481adc8cb9eb9743585d9cbecd97cf8c58a
Bilyana17 <biljana.bicka@gmail.com>
2024-03-24 18:09:50 -0500

    Add objects to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-alert('Hello world');
-
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+//Creating variables
+let pokemonList = [];
+pokemonList = [{name:"balbasure", height: 7, type: ["grass", "poison"] }, { name: "butterfree", height: 1, type: ["bug", "flying"] }, { name: "onix", height: 8, type: ["rock", "ground"] }];
