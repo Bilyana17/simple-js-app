d565a137117dadc737ba3445d817a12aff51f345
Bilyana17 <biljana.bicka@gmail.com>
2024-05-26 23:13:20 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,50 @@
 //Creating variables
+let pockemonRepository = (function () {
 
-let pokemonList = [
-    {
-        name: "balbasura",
-        height: 7,
-        type: ["grass", "poison"]
-    },
-    {
-        name: "butterfree",
-        height: 1,
-        type: ["bug", "flying"]
-    },
-    {
-        name: "onix",
-        height: 8,
-        type: ["rock", "ground"]
+    let pokemonList = [
+        {
+            name: "balbasura",
+            height: 7,
+            type: ["grass", "poison"]
+        },
+        {
+            name: "butterfree",
+            height: 1,
+            type: ["bug", "flying"]
+        },
+        {
+            name: "onix",
+            height: 8,
+            type: ["rock", "ground"]
+        }
+    ];
+
+    /*for (let i = 0; 1 < pokemonList.length; i++) {
+        let displayText = pokemonList[i].name + " (height: " + pokemonList[i].height + ")";
+        if (pokemonList[i].height > 7) {
+            displayText += " - It's a bug!";
+        } else if (pokemonList[i].height === 7) {
+            displayText += " - It's a turtule!";
+        } else {
+            displayText += " - It's a rock!"
+        }
+    
+        document.write(displayText + "<br>");
+    }*/
+
+    function getAll() {
+        return pokemonList;
     }
-];
-
-/*for (let i = 0; 1 < pokemonList.length; i++) {
-    let displayText = pokemonList[i].name + " (height: " + pokemonList[i].height + ")";
-    if (pokemonList[i].height > 7) {
-        displayText += " - It's a bug!";
-    } else if (pokemonList[i].height === 7) {
-        displayText += " - It's a turtule!";
-    } else {
-        displayText += " - It's a rock!"
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
+    return {
+        getAll,
+        add: add
+    }
+})();
 
-    document.write(displayText + "<br>");
-}*/
+console.log(pockemonRepository.getAll());
 
 pokemonList.forEach(function (user) {
     console.log(user.name + '  ' + user.height + '  ' + user.type);
